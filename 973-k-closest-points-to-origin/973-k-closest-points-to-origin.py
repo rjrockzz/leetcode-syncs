02776e0b1487e9a0288a90e16376eb6a7cbcdc1a
codιng вloĸe <rjrockzz@protonmail.com>
2022-09-18 02:07:47 +0530

    Time: 2087 ms (7.48%), Space: 20.3 MB (61.70%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,8 +1,9 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         hashmap = []
+        heap = answer = []
         for i in points:
-            hashmap.append([i,(i[0]**2) + (i[1]**2)])
-        hashmap.sort(key = lambda x : x[1])
-        return [list(keys[0]) for keys in hashmap][:k]
+            hashmap.append([((i[0]**2) + (i[1]**2)), i])
+        heapq.heapify(hashmap)
+        return [heapq.heappop(hashmap)[1] for _ in range(k)]
             
