cbfcc2a0ba1ece435bd3e841279afcc8a3a8f764
codιng вloĸe <rjrockzz@protonmail.com>
2022-09-23 23:38:08 +0530

    Time: 355 ms (25.98%), Space: 17.5 MB (71.59%) - LeetHub

---
+++ 384-shuffle-an-array/384-shuffle-an-array.py
@@ -0,0 +1,11 @@
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.reset = lambda:nums
+        self.shuffle = lambda: random.sample(nums, len(nums))
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
