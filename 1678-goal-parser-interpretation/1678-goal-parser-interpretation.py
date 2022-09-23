89fcb67bb140a1416a62632899b57f934f809053
codιng вloĸe <rjrockzz@protonmail.com>
2022-09-23 10:09:55 +0530

    Time: 57 ms (30.16%), Space: 13.8 MB (55.18%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,15 +1,15 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        pointer = 0
-        result = ''
-        while pointer < len(command):
-            if command[pointer] == 'G':
-                result += "G"
-                pointer += 1
-            elif command[pointer:pointer+2] == '()':
-                result += "o"
-                pointer += 2
-            else:
-                result += "al"
-                pointer += 4
-        return result
+        ans = str_builder = ""
+        for i in command:
+            str_builder+=i
+            if str_builder=="G":
+                ans+="G"
+                str_builder=""
+            if str_builder=="()":
+                ans+="o"
+                str_builder=""
+            if str_builder == "(al)":
+                ans+="al"
+                str_builder=""
+        return ans
