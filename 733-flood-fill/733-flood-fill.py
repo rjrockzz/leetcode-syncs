457571823bc80d2d778f03cf820b59607c0cbcb4
codιng вloĸe <rjrockzz@protonmail.com>
2022-09-30 10:19:32 +0530

    Time: 164 ms (24.16%), Space: 14.2 MB (38.45%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,12 +1,29 @@
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+    def floodFill(self, image, sr, sc, newColor):
         def dfs(i, j):
-            image[i][j] = color
-            for x, y in ((i-1,j),(i+1,j),(i,j-1),(i,j+1)):
-                if 0<=x<m and 0<=y<n and image[x][y]==old:
-                    dfs(x,y)
-                       
-        old, m , n = image[sr][sc], len(image), len(image[0])
-        if old!=color:
-            dfs(sr,sc)
+            image[i][j] = newColor
+            for x, y in ((i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)): # R-1, R+1, C-1, C+1
+                '''
+                4-D Pixel Traversal :O
+                '''
+                if 0 <= x < m and 0 <= y < n and image[x][y] == old: # Consists of the 2 dimensions
+                    '''
+                    We're checking whether the coordinates actually lie within the image
+                    and then we compare IF THE COLOR is actually the same as the one we had previously
+                    AND As soon as we have the condition satisfied, we move forward!
+                    '''
+                    dfs(x, y)
+        '''
+        To keep our limits in check about the pixel dimensions, we also assign m, n.
+        '''
+        old, m, n = image[sr][sc], len(image), len(image[0]) 
+        '''
+        old is the color of original pixel, length of the row (m), length of the column (n)
+        '''
+        if old != newColor: 
+            dfs(sr, sc)
+        '''
+        If the old color is actually equal to the new color, flood fill will NOT proceed,
+        because there are no changes initially only.
+        '''
         return image
