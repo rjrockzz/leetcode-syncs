8e2b7f017ab14114e4435b89007ea847d183aa99
codιng вloĸe <rjrockzz@protonmail.com>
2022-10-25 13:18:33 +0530

    Time: 1288 ms (33.00%), Space: 50.3 MB (16.21%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.py
+++ 0815-bus-routes/0815-bus-routes.py
@@ -1,37 +1,42 @@
-'''
-Explanation:
-* The first part loop on routes and record stop to routes mapping in to_route.
-* The second part is general bfs. Take a stop from queue and find all connected route.
-* The hashset seen record all visited stops and we won't check a stop for twice.
-* We can also use a hashset to record all visited routes, or just clear a route after visit.
-'''
-import collections
 class Solution:
-    def numBusesToDestination(self, routes, source: int, target: int) -> int:
-        # mapping of stops to the bus
-        to_routes = collections.defaultdict(set)
-        for i, route in enumerate(routes):
-            for j in route:
-                to_routes[j].add(i) 
-        '''
-        final result is like let's suppose for bus 0 the routes were like [1,2,7], so the to_routes mapping comes out to be (defaultdict with set as the default type) {1 : {0}, 2: {0}, 7:{0}} and for the bus 1 it will be {1 : {0}, 2: {0},3:{1},6:{1}, 7:{0,1}}
-        '''
-        bfs = [(source, 0)]
-        '''
-        initially we intitialise it to [(1, 0)], where 1 is the source and the 0 is the bus to be taken, init bus = 0 that means we haven't boarded any bus as of yet.
-        '''
-        seen = set([source]) # since we're already starting with the source, we can mark it as visited.
-        for stop, bus in bfs: # intially stop = 1, bus = 0
-            if stop == target: # stopping condition.
-                return bus
-            for i in to_routes[stop]: # stop initially 1, ie. to_routes[stop] = 0
-                for j in routes[i]: # now traversing through all the routes for that particular bus, since now we know that the bus will definitely stop at that particular stop, so we carry on visiting the routes it will now further take in the course of the journey.
-
-                    if j not in seen: # we check if we have already visited that stop before, it means it's of no use visiting the same stop again in the further steps.
-                        bfs.append((j, bus+1)) 
-                        seen.add(j)
-                # we mark this as empty list as soon as we have visited using a particular bus point.
-                routes[i] = []                 
+    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        # Since we have to visit each stops using the buses also see
+        # WE SHOULD NOT USE routes instead of the buses for BFS because of the constraints
+        # Using BFS is kind of the go-to here since we have to find the minimum
+        # number of buses, that is the shortest path to reach a particular stop.
+        
+        # our base condition can be to simply return the stop if we're already at the 
+        # start == stop
+        if source==target:
+            return 0
+        
+        routes_to_bus = collections.defaultdict(set)
+        # we're now creating a mapping of the routes -> bus, since now we will 
+        # be performing our BFS on the buses instead of the stops due to the constraints.
+        for index,i in enumerate(routes):
+            for j in i:
+                routes_to_bus[j].add(index)
+        # Now since we're moving forward to our BFS, let's initialize 
+        # some of our variables.
+        seen_stops = set()
+        seen_buses = set()
+        queue = deque([(source, 0)])
+        
+        # now we can proceed for our bfs
+        while queue:
+            stop, bus_count = queue.popleft()
+            if stop==target:
+                return bus_count
+            
+            for bus_number in routes_to_bus[stop]: # traversing through the buses
+                if bus_number not in seen_buses:
+                    seen_buses.add(bus_number)
+                    # now we traverse the stops created by the buses
+                    for stop_of_the_bus_number in routes[bus_number]:
+                        if stop_of_the_bus_number not in seen_stops:
+                            seen_stops.add(stop_of_the_bus_number)
+                            queue.append((stop_of_the_bus_number, bus_count+1))
         return -1
-x = Solution()
-print(x.numBusesToDestination(routes = [[1,2,7],[3,6,7]], source = 1, target = 6))
+            
+                
+        
