3588a575a19572bfdb803453ba6e1ca74b9e6b45
codιng вloĸe <rjrockzz@protonmail.com>
2022-10-26 11:16:21 +0530

    Time: 237 ms (6.64%), Space: 26.4 MB (31.00%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -4,13 +4,7 @@ class TreeNode:
         self.val = x
         self.left = None
         self.right = None
-'''
-Please note that p and q always exist in the tree.
-* Since we dfs from the root down to its children, if current root == p or root == q then current root must be   their LCA.
-* If left subtree contains one of descendant (p or q) and right subtree contains the remaining descendant (q     or p) then the root is their LCA.
-* If left subtree contains both p and q then return left as their LCA.
-* If right subtree contains both p and q then return right as their LCA.
-'''
+
 class Solution:
     def lowestCommonAncestor(self, root, p, q):
         # O(N)
@@ -21,22 +15,50 @@ class Solution:
         :rtype: TreeNode
         """
         '''
-        given this tree, let's suppose p = 6 , q = 1
+        given this tree, let's suppose p = 6 , q = 4
         
         
                     3
                   /   \
-                 5     1
-                / \
-               6   2
-                  / \
+                [5]----1--------> since here we found both the left and the right to be not None, we return!  
+      return 6  / \ return 4
+               6   2 --------------> we now check here for whether we have a left or right to be not None.
+      return null / \ return 4  
                  7   4
         
         '''
-        if root == None or root == p or root == q: return root
+        # this is the base condition, where if we find with of the None, p or q, we 
+        # return the found value.
+        if root == None or root == p or root == q: 
+            return root
+        # we start with out dfs
         left = self.lowestCommonAncestor(root.left, p, q)
         right = self.lowestCommonAncestor(root.right, p, q)
+        # if we arrive at a node like 5 , where both left and right is not null, here we can say
+        # that we've reached the LCA of both the given nodes
         if left != None and right != None: return root
+        # if the left subtree contains both the nodes, we return the left
         if left != None: return left
+        # and we return right if the right subtree contains both these nodes.
         return right
         
+
+
+        '''
+        1. Base Condition
+        2. DFS
+        3. Perform the operations we want
+        '''
+        
+
+root = TreeNode(3)
+root.left = TreeNode(5)
+root.right = TreeNode(1)
+root.left.left = TreeNode(6)
+root.left.right = TreeNode(2)
+root.left.right.left = TreeNode(7)
+root.left.right.right = TreeNode(4)
+root.right.left = TreeNode(0)
+root.right.right = TreeNode(8)
+
+print(Solution().lowestCommonAncestor(root, root.left.left, root.right.left).val)
