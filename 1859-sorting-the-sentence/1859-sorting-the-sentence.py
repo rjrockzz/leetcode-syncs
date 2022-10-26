2fa2de5eb2ec143a7af45b27ac593375d9fd3eae
codιng вloĸe <rjrockzz@protonmail.com>
2022-10-26 20:16:03 +0530

    Time: 43 ms (76.09%), Space: 13.9 MB (58.59%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,8 +1,12 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
         lst = s.split(" ")
+        # It's important that we first create the new list, and assign the Nones
+        # until the length of the 
         result = [None] * (len(lst)+1)
         for i in lst:
+            # selecting the last element of list -> lst[-1]
+            # selecting everything except the last element -> lst[:-1]
             result[int(i[-1])] = i[:-1]
         result.pop(0)
         return " ".join(result)
