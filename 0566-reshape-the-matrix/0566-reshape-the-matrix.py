fa2b32450ec9ae319de39ab33363a1dff2a41dc6
codιng вloĸe <rjrockzz@protonmail.com>
2022-11-07 15:24:35 +0530

    Time: 298 ms (6.74%), Space: 32.7 MB (6.02%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class Solution(object):
+    def matrixReshape(self, nums, r, c):
+        try:
+            return np.reshape(nums, (r, c)).tolist()
+        except:
+            return nums
