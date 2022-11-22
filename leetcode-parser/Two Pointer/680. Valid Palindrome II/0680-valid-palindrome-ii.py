aa2461db6ee46f91e4f61340c49d3283e0e66b73
codιng вloĸe <rjrockzz@protonmail.com>
2022-11-22 10:11:46 +0530

    committing files

---
+++ leetcode-parser/Two Pointer/680. Valid Palindrome II/0680-valid-palindrome-ii.py
@@ -0,0 +1,20 @@
+'''
+We can use the standard two-pointer approach that starts at the left and right of the string and move inwards. Whenever there is a mismatch, we can either exclude the character at the left or the right pointer. We then take the two remaining substrings and compare against its reversed and see if either one is a palindrome.
+
+\U0001f4af Check out https://www.techinterviewhandbook.org for more tips and tricks by me to ace your coding interview \U0001f4af
+'''
+class Solution(object):
+    def validPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # Time: O(n)
+        # Space: O(n)
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                one, two = s[left:right], s[left + 1:right + 1]
+                return one == one[::-1] or two == two[::-1]
+            left, right = left + 1, right - 1
+        return True
