3eca684186e54e0bc744c1f5598db22519ec8262
codιng вloĸe <rjrockzz@protonmail.com>
2022-11-28 09:25:00 +0530

    Time: 9018 ms (5.06%), Space: 70.6 MB (6.70%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -1,12 +1,21 @@
+# From beginning we know that we have to apply sorting, which automatically gets O(nlogn) time complexity. Find a way from the start where we can let go of the sorting altogether. That's where Counting Sort comes into play
 class Solution:
-    def findWinners(self, matches):
-        winner_hashmap = {}
-        loser_hashmap = {}
-        answer = []
-        for i in matches:
-            winner_hashmap[i[0]] = 1 + winner_hashmap.get(i[0], 0)
-            loser_hashmap[i[1]] = 1 + loser_hashmap.get(i[1], 0)
-        # for no losses, we'll take a set difference between the winner and the loser hashmap
-        answer.append(sorted(list(set([*winner_hashmap]) - set([*loser_hashmap]))))
-        answer.append(sorted([k for k, v in loser_hashmap.items() if v == 1]))
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        losses_count = [-1] * 100001
+
+        for winner, loser in matches:
+            if losses_count[winner] == -1:
+                losses_count[winner] = 0
+            if losses_count[loser] == -1:
+                losses_count[loser] = 1
+            else:
+                losses_count[loser] += 1
+            
+        answer = [[], []]
+        for i in range(100001):
+            if losses_count[i] == 0:
+                answer[0].append(i)
+            elif losses_count[i] == 1:
+                answer[1].append(i)
+                
         return answer
