4b57cc4ed224cc354e08ad3d673b3a62bc61fa41
codιng вloĸe <rjrockzz@protonmail.com>
2022-12-11 18:22:56 +0530

    Time: 531 ms (21.08%), Space: 0B (100.00%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,21 @@
+# Write your MySQL query statement below
+/*
+Seat
+* id
+* student
+id is the PK, continuous increment.
+*/
+SELECT
+    (CASE
+        WHEN MOD(id, 2) != 0 AND counts != id THEN id + 1
+        WHEN MOD(id, 2) != 0 AND counts = id THEN id
+        ELSE id - 1
+    END) AS id,
+    student
+FROM
+    seat,
+    (SELECT
+        COUNT(*) AS counts
+    FROM
+        seat) AS seat_counts
+ORDER BY id ASC;
