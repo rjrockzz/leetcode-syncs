5b140fee3b453c1b62278abd9c1cdd93607ce3d9
codιng вloĸe <rjrockzz@protonmail.com>
2022-12-12 13:23:51 +0530

    committing files

---
+++ leetcode-parser/SQL/1164. Product Price At A Given Date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,37 @@
+# Write your MySQL query statement below
+/* 
+Products
+* product_id
+* new_price
+* change_date
+(product_id, change_date) is the PK for the table
+
+initial price is 10
+*/
+SELECT
+  product_id,
+  10 AS price
+FROM
+  Products
+GROUP BY
+  product_id
+HAVING
+  MIN(change_date) > '2019-08-16'
+UNION ALL
+SELECT
+  product_id,
+  new_price AS price
+FROM
+  Products
+WHERE
+  (product_id, change_date) IN (
+    SELECT
+      product_id,
+      MAX(change_date)
+    FROM
+      Products
+    WHERE
+      change_date <= '2019-08-16'
+    GROUP BY
+      product_id
+  )
