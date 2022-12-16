56666d9fa5fc9f88ed6cc1c97ab3df0fe5248074
codιng вloĸe <rjrockzz@protonmail.com>
2022-12-16 19:33:39 +0530

    committing files

---
+++ leetcode-parser/SQL/1113. Reported Posts/1113-reported-posts.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select extra as report_reason,count(distinct post_id) as report_count from actions where action = "report" and action_date = "2019-07-04" group by 1
