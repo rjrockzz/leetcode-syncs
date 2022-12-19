bf3e3391d4f00dd926712622c55478fb3c46495b
codιng вloĸe <rjrockzz@protonmail.com>
2022-12-19 20:54:37 +0530

    committing files

---
+++ leetcode-parser/SQL/1435. Create A Session Bar Chart/1435-create-a-session-bar-chart.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+WITH cte AS (
+    SELECT '[0-5>' AS bin,  0 AS min_duration, 5*60 AS max_duration
+    UNION ALL
+    SELECT '[5-10>' AS bin,  5*60 AS min_duration, 10*60 AS max_duration
+    UNION ALL
+    SELECT '[10-15>' AS bin, 10*60 AS min_duration, 15*60 AS max_duration
+    UNION ALL
+    SELECT '15 or more' AS bin,  15*60 as min_duration, 2147483647 AS max_duration
+    )
+
+SELECT cte.bin, COUNT(s.session_id) AS total
+FROM Sessions s
+RIGHT JOIN cte 
+		ON s.duration >= min_duration 
+        AND s.duration < max_duration				 
+GROUP BY cte.bin;
