8066ecd2289c49721d8d5c786f1ac38834ee9903
codιng вloĸe <rjrockzz@protonmail.com>
2022-12-22 19:00:03 +0530

    Time: 1504 ms (18.62%), Space: 0B (100.00%) - LeetHub

---
+++ 1809-ad-free-sessions/1809-ad-free-sessions.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+/*
+Playback
+* session_id
+* customer_id
+* start_time
+* end_time
+
+Ads
+* ad_id
+* customer_id
+* timestamp
+*/
+SELECT session_id
+FROM playback pb LEFT JOIN ads ad
+ON pb.customer_id = ad.customer_id
+AND ad.timestamp BETWEEN start_time and end_time
+WHERE ad.customer_id IS NULL
