3b03bd22a7ddfadb25fd5b2d657c75fdc80c67be
codιng вloĸe <rjrockzz@protonmail.com>
2022-12-24 18:18:33 +0530

    committing files

---
+++ leetcode-parser/SQL/2346. Compute The Rank As A Percentage/2346-compute-the-rank-as-a-percentage.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT student_id, department_id, 
+    ROUND(100*PERCENT_RANK() OVER (
+          PARTITION BY department_id 
+          ORDER BY mark DESC)
+    , 2) AS percentage 
+FROM Students
