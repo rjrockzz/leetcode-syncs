be9e63caefaa57a06b9d7d16843f3385b6ac3751
codιng вloĸe <rjrockzz@protonmail.com>
2022-12-24 18:01:53 +0530

    committing files

---
+++ leetcode-parser/SQL/2066. Account Balance/2066-account-balance.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+SELECT account_id, day, sum(case when type = 'Deposit' then amount else -amount end) over(partition by account_id order by day asc) as balance 
+FROM transactions
