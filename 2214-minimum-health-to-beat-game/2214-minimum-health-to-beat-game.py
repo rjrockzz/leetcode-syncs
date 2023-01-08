236a35ba05f2f73338250f030b7ae519f24df1cc
codιng вloĸe <rjrockzz@protonmail.com>
2023-01-08 09:39:39 +0530

    Time: 611 ms (72.22%), Space: 27.6 MB (82.37%) - LeetHub

--- 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.py
+++ 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.py
@@ -1,3 +1,13 @@
+'''
+Greedy:
+Approach: Greedy
+Intuition
+We can see that the total damage that can be caused in the game is the sum of all the elements of the damage array. Intuitively, we can also realize that among all the damages caused at every level, the armor should be used to block the largest damage, let's say this largest damage is d. The armor could either completely block the damage if armor >= d or partially block it if armor < d. This means we can block min(armor, d) damage with the armor. We get the net damage suffered in the game by subtracting the amount of damage blocked by the armor from the total damage.
+
+Following this discussion, we iterate over all the elements of the damage array and compute the sum of its elements, let's call it totalDamage. While iterating over the elements, we also find the largest element of the damage array, let's call it maxDamage. The amount of damage blocked after using the armor (at the level with damage equal to maxDamage) would be min(armor, maxDamage). As a result, the total damage dealt throughout the game is totalDamage - min(armor, MaxDamage).
+
+In the end, we return totalDamage - min(armor, maxDamage) + 1 as the answer.
+'''
 class Solution:
     def minimumHealth(self, damage: List[int], armor: int) -> int:
         '''
