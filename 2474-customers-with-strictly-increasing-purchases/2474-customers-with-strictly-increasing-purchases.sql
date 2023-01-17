74ec67faecb8318398b41e7ddc70d9b854e942a7
codιng вloĸe <rjrockzz@protonmail.com>
2023-01-17 11:57:20 +0530

    Time: 1231 ms (9.54%), Space: 0B (100.00%) - LeetHub

--- 2474-customers-with-strictly-increasing-purchases/2474-customers-with-strictly-increasing-purchases.sql
+++ 2474-customers-with-strictly-increasing-purchases/2474-customers-with-strictly-increasing-purchases.sql
@@ -1,12 +1,16 @@
 # Write your MySQL query statement below
--- purchase of each customer by year
-with yearly as
-(select customer_id, year(max(order_date)) year, sum(price) price
-from orders
-group by year(order_date),customer_id)
--- "left" join each year with its +1 year, and has strictly bigger purchase on +1 year.
-select y1.customer_id
-from yearly y1
-left join yearly y2 on y1.customer_id=y2.customer_id and y1.year+1=y2.year and y1.price<y2.price
-group by y1.customer_id
-having count(*)-count(y2.customer_id)=1
+/* Orders
+* order_id
+* customer_id
+* order_date
+* price
+
+Catch -> We need to add / consider the years in between, if we have an year coming out to be 2015 and the next year is 2017 , we should consider the value for 2016 as 0
+*/
+with yearly_prices as (SELECT customer_id,  DATE_FORMAT(order_date, "%Y") year, sum(price) as total_price FROM Orders group by 1,2),
+inc_dec as (
+select * , 
+ case when total_price > LAG(total_price,1,-1) over (partition by customer_id order by year) and lag(year) over (partition by customer_id order by year)  = year - 1
+    then 0 ELSE 1 END as increasing_decreasing
+from yearly_prices)
+select customer_id from inc_dec group by customer_id having sum(increasing_decreasing)=1 
