c149c791dc36ef830498df17ce04d1772562af19
codιng вloĸe <rjrockzz@protonmail.com>
2023-01-17 11:58:38 +0530

    Time: 1697 ms (5.03%), Space: 0B (100.00%) - LeetHub

--- 2474-customers-with-strictly-increasing-purchases/2474-customers-with-strictly-increasing-purchases.sql
+++ 2474-customers-with-strictly-increasing-purchases/2474-customers-with-strictly-increasing-purchases.sql
@@ -10,7 +10,7 @@ Catch -> We need to add / consider the years in between, if we have an year comi
 with yearly_prices as (SELECT customer_id,  DATE_FORMAT(order_date, "%Y") year, sum(price) as total_price FROM Orders group by 1,2),
 inc_dec as (
 select * , 
- case when total_price > LAG(total_price,1,-1) over (partition by customer_id order by year) and lag(year) over (partition by customer_id order by year)  = year - 1
+ case when total_price > LAG(total_price) over (partition by customer_id order by year) and lag(year) over (partition by customer_id order by year)  = year - 1
     then 0 ELSE 1 END as increasing_decreasing
 from yearly_prices)
 select customer_id from inc_dec group by customer_id having sum(increasing_decreasing)=1 
