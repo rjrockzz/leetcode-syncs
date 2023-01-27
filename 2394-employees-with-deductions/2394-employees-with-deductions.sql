44853bc48030bdaac0847a9157606f67ab73c608
codιng вloĸe <rjrockzz@protonmail.com>
2023-01-27 15:20:05 +0530

    Time: 1706 ms (27.22%), Space: 0B (100.00%) - LeetHub

--- 2394-employees-with-deductions/2394-employees-with-deductions.sql
+++ 2394-employees-with-deductions/2394-employees-with-deductions.sql
@@ -1,11 +1,17 @@
 # Write your MySQL query statement below
-SELECT
-  E.employee_id
-FROM
-  Employees E
-  LEFT JOIN Logs L ON E.employee_id = L.employee_id
-GROUP BY
-  E.employee_id,
-  E.needed_hours
-HAVING
-  (SUM(CEIL(IFNULL(TIMESTAMPDIFF(SECOND, L.in_time, L.out_time),0) / 60)) / 60) < E.needed_hours;
+/*
+Employees
+* employee_id
+* needed_hours
+Each row contains the id of an employee and the minimum number of hours needed for them to work to get their salary.
+
+Logs 
+* employee_id
+* in_time
+* out_time
+*/
+with cte as
+(
+    select employee_id, CEILING((sum(timestampdiff(SECOND, in_time, out_time)))/60) as total_time from logs group by 1
+)
+select e.employee_id from employees e left join cte c using(employee_id) where needed_hours*60>ifnull(total_time,0)
