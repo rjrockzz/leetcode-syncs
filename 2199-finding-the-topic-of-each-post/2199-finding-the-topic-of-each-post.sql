429261f6ff4cd80d53fd0d3cbfcaeaa475b4c122
codιng вloĸe <rjrockzz@protonmail.com>
2023-02-02 13:54:17 +0530

    Time: 1533 ms (56.82%), Space: 0B (100.00%) - LeetHub

--- 2199-finding-the-topic-of-each-post/2199-finding-the-topic-of-each-post.sql
+++ 2199-finding-the-topic-of-each-post/2199-finding-the-topic-of-each-post.sql
@@ -1,73 +1,19 @@
-# CTEs
-WITH RECURSIVE words_count_in_content AS (
-    # 1. Update `Posts` table with column `num_of_words` showing count of words in `content`
-    SELECT
-        post_id
-        , content
-        , CHAR_LENGTH(content)
-        - CHAR_LENGTH(
-            REPLACE(content, ' ', '')
-        )
-        + 1 AS num_of_words
-    FROM
-        Posts
-)
-, max_words_count_in_content AS (
-    # 2. Calculate max words number among all comments from `Posts` table
-    SELECT
-        MAX(num_of_words) AS max_num_of_words
-    FROM
-        words_count_in_content
-)
-, numbers AS (
-    # 3. Recursion: create a series of integers from 1 to `max_num_of_words` with increment = 1
-    SELECT
-        1 AS num
-    UNION ALL
-    SELECT
-        num + 1
-    FROM
-        numbers
-    WHERE
-        num < (
-            # condition to stop recursion
-            SELECT
-                max_num_of_words
-            FROM
-                max_words_count_in_content
-        )
-)
-, posts_and_separate_words AS (
-    # 4. Create long table with primary key of (`post_id`, `word`)
-    SELECT
-        w.post_id
-        , SUBSTRING_INDEX(
-            SUBSTRING_INDEX(w.content, ' ', n.num),
-            ' ',
-            -1
-         ) AS word
-    FROM
-        words_count_in_content AS w
-            INNER JOIN numbers AS n
-                ON w.num_of_words >= n.num
-)
-# Query's body
-# 5. Calculate target result: convert `topic_id` to string, keep unique values only, use string aggregation for each post_id, if no corresponding topics impute NULL with string 'Ambiguous!'
-SELECT
-    p.post_id
-    , COALESCE(
-        GROUP_CONCAT(
-            DISTINCT CONVERT(k.topic_id, char)
-            ORDER BY k.topic_id ASC
-            SEPARATOR ','
-        ),
-        'Ambiguous!'
-    ) AS topic
-FROM
-    posts_and_separate_words AS p
-        LEFT JOIN Keywords AS k # we use LEFT JOIN here to handle cases where there is no match in words for particular `topic_id`
-            ON p.word = k.word
-GROUP BY
-    p.post_id
-ORDER BY
-    p.post_id ASC;
+# Write your MySQL query statement below
+/*
+Keywords
+* topic_id
+* word
+Each row of this table contains the id of a topic and a word that is used to express this topic.
+There may be more than one word to express the same topic and one word may be used to express multiple topics.
+
+Posts
+* post_id
+* content
+*/
+SELECT 
+    P.post_id, 
+    IFNULL(GROUP_CONCAT(DISTINCT K.topic_id ORDER BY K.topic_id), 'Ambiguous!') AS topic
+FROM Posts AS P
+LEFT JOIN Keywords AS K
+ON CONCAT(' ', LOWER(P.content), ' ') LIKE CONCAT('% ', LOWER(K.word), ' %')
+GROUP BY P.post_id
