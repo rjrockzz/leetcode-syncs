57d90bf87250f6197065243170e7fed59a6a55ed
codιng вloĸe <rjrockzz@protonmail.com>
2023-02-02 14:40:26 +0530

    Time: 838 ms (18.34%), Space: 0B (100.00%) - LeetHub

--- 2228-users-with-two-purchases-within-seven-days/2228-users-with-two-purchases-within-seven-days.sql
+++ 2228-users-with-two-purchases-within-seven-days/2228-users-with-two-purchases-within-seven-days.sql
@@ -1,7 +1,8 @@
 # Write your MySQL query statement below
-select  distinct user_id
-from
-( select user_id, purchase_date,
-lag(purchase_date) over (partition by user_id order by purchase_date) prev_purchase_date
-from purchases) t
-where datediff(purchase_date, prev_purchase_date) <=7
+/*
+Purchases
+* purchase_id
+* user_id
+* purchase_date
+*/
+select distinct p_left.user_id from purchases p_left join purchases p_right on ABS(datediff(p_left.purchase_date, p_right.purchase_date))<=7 and p_left.user_id = p_right.user_id and p_left.purchase_id!= p_right.purchase_id order by p_left.user_id
