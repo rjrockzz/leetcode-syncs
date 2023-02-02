af5fec973e946e77196e0c49cbff1277f35dad72
codιng вloĸe <rjrockzz@protonmail.com>
2023-02-02 13:18:54 +0530

    Time: 1339 ms (21.77%), Space: 0B (100.00%) - LeetHub

--- 2173-longest-winning-streak/2173-longest-winning-streak.sql
+++ 2173-longest-winning-streak/2173-longest-winning-streak.sql
@@ -1,35 +1,19 @@
 # Write your MySQL query statement below
-WITH base_data AS (
-  SELECT
-    M1.*,
-    ROW_NUMBER() OVER (PARTITION BY M1.player_id ORDER BY M1.match_day) AS match_seq_num
-  FROM
-    Matches M1
-), grps AS (
-  SELECT
-    BD.*,
-    BD.match_seq_num - ROW_NUMBER() OVER (PARTITION BY BD.player_id ORDER BY BD.match_day) AS grp
-  FROM
-    base_data BD
-  WHERE
-    BD.result = 'Win'
-), consec_counts AS (
-  SELECT
-    G.player_id,
-    COUNT(G.grp) AS consec_count,
-    ROW_NUMBER() OVER(PARTITION BY G.player_id ORDER BY COUNT(G.grp) DESC) AS consec_rnk
-  FROM
-    grps G
-  GROUP BY
-    G.player_id, G.grp
-)
-SELECT
-  M2.player_id,
-  (CASE WHEN CC.player_id IS NULL THEN 0 ELSE CC.consec_count END) AS longest_streak
-FROM
-  Matches M2
-  LEFT JOIN consec_counts CC ON M2.player_id = CC.player_id
-WHERE
-  CC.consec_rnk = 1 OR CC.player_id IS NULL
-GROUP BY
-  M2.player_id, longest_streak;
+/*
+Matches 
+* player_id
+* match_id
+* result
+
+We have to find the longest Winning Streak for each of the players
+*/
+with cte as
+(
+  select
+    player_id, match_day, result, 
+    DENSE_RANK() over(partition by player_id order by match_day) - 
+    DENSE_RANK() over(partition by player_id,result order by match_day) as sequence_groupings    
+    from matches
+) , final as (select player_id, count(*) as win_counts from cte where result = "Win" group by player_id, sequence_groupings)
+select distinct m.player_id, ifnull(max(win_counts),0) as longest_streak from matches m left join final using (player_id) group by 1 
+
