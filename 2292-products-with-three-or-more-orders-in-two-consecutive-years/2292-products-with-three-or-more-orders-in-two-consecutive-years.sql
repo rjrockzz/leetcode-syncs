41c962b1eab25b700cf88c9589f1ac10a51c1d46
codιng вloĸe <rjrockzz@protonmail.com>
2023-02-02 15:22:50 +0530

    Time: 899 ms (42.86%), Space: 0B (100.00%) - LeetHub

--- 2292-products-with-three-or-more-orders-in-two-consecutive-years/2292-products-with-three-or-more-orders-in-two-consecutive-years.sql
+++ 2292-products-with-three-or-more-orders-in-two-consecutive-years/2292-products-with-three-or-more-orders-in-two-consecutive-years.sql
@@ -1,9 +1,14 @@
 # Write your MySQL query statement below
-SELECT DISTINCT d.product_id FROM
-(SELECT product_id, 
- YEAR(purchase_date) AS curr_year,
- LEAD(YEAR(purchase_date)) OVER(PARTITION BY product_id ORDER BY YEAR(purchase_date)) AS next_year
- FROM orders
- GROUP BY curr_year, product_id
- HAVING COUNT(order_id) >= 3) d
- WHERE d.next_year=d.curr_year+1
+/*
+Orders
+* order_id
+* product_id
+* quantity
+* purchase_date
+
+Products that were ordered three or more times in two consecutive years.
+*/
+with cte as
+(
+    select product_id, left(purchase_date, 4) as years from orders group by 1,2 having count(order_id)>=3
+)select distinct c1.product_id from cte c1 join cte c2 on c1.years+1=c2.years and c1.product_id = c2.product_id
