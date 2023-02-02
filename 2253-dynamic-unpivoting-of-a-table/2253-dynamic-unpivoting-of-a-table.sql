ff392e4b90c63fce9d141add54ced0a5ca978dfb
codιng вloĸe <rjrockzz@protonmail.com>
2023-02-02 15:09:14 +0530

    Time: 2225 ms (34.92%), Space: 0B (100.00%) - LeetHub

--- 2253-dynamic-unpivoting-of-a-table/2253-dynamic-unpivoting-of-a-table.sql
+++ 2253-dynamic-unpivoting-of-a-table/2253-dynamic-unpivoting-of-a-table.sql
@@ -1,19 +1,23 @@
+/*
+Products
+* Column Name
+* Type
+Each row in this table indicates the product's price in n different stores.
+If the product is not available in a store, the price will be null in that store's column.
+The names of the stores may change from one testcase to another. There will be at least 1 store and at most 30 stores.
+*/
 CREATE PROCEDURE UnpivotProducts()
 BEGIN
-    SET SESSION group_concat_max_len = 1000000;
-    SET @case_stmt = NULL;
-
-	SELECT GROUP_CONCAT(
-            'SELECT product_id, "', COLUMN_NAME, '" AS store, ', COLUMN_NAME, ' AS price FROM Products WHERE ', COLUMN_NAME,' IS NOT NULL' SEPARATOR " UNION "
+	set session group_concat_max_len = 198742918;
+    set @variable = null;
+	select group_concat(
+        concat(
+            'select product_id, "',`column_name`, '" as store, ', `column_name`, ' as price from products where ',`column_name`, ' is not null') separator ' union '
         )
-    INTO @case_stmt
-    FROM INFORMATION_SCHEMA.COLUMNS
-    WHERE TABLE_NAME="Products" AND COLUMN_NAME != "product_id";
-
-    SET @sql_query = @case_stmt;
-
-    PREPARE final_sql_query FROM @sql_query;
-    EXECUTE final_sql_query;
-    DEALLOCATE PREPARE final_sql_query;
-
+        into @variable
+        from `information_schema`.`columns`
+        where `table_schema` = 'test' and `table_name` = 'products' and `column_name` != 'product_id';
+        prepare sql_query from @variable;
+        execute sql_query;
+        deallocate prepare sql_query;
 END
