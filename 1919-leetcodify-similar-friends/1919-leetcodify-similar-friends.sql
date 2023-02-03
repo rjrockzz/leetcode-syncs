d3e57cef5d3bb569844a0c454fab189abfa4d473
codιng вloĸe <rjrockzz@protonmail.com>
2023-02-03 10:16:08 +0530

    Time: 5060 ms (8.47%), Space: 0B (100.00%) - LeetHub

--- 1919-leetcodify-similar-friends/1919-leetcodify-similar-friends.sql
+++ 1919-leetcodify-similar-friends/1919-leetcodify-similar-friends.sql
@@ -1,18 +1,21 @@
 # Write your MySQL query statement below
 /*
-The idea is to
+Listens
+* user_id
+* song_id
+* day
 
-find distinct combos of (user1, user2, day)
-make sure they are also in the Friendship table (existing friendship)
-filter to make sure that there are over 3 songs in overlap
-Below is the code.
+Friendship
+* user1_id
+* user2_id
+user1_id < user2_id
+
+Concept: Mapping Question, wish we could use a Hashmap haha.
 */
-SELECT DISTINCT a.user_id AS user1_id, b.user_id AS user2_id # 1) Find distinct combos
-FROM Listens a, Listens b
-WHERE a.day = b.day
-	AND a.song_id = b.song_id
-	AND a.user_id != b.user_id
-	AND (a.user_id, b.user_id) IN (SELECT * FROM Friendship) # 2) They are also in Friendship table
-	
-GROUP BY a.user_id, b.user_id, b.day
-HAVING COUNT(DISTINCT a.song_id) >= 3 # 3) There are >= 3 overlaps
+select distinct A.user_id as user1_id, B.user_id as user2_id from listens A, listens B
+where A.day = B.day AND
+A.song_id = B.song_id AND
+A.user_id!= B.user_id AND
+(A.user_id, B.user_id) IN (select * from friendship)
+GROUP BY a.user_id, b.user_id, a.day
+having COUNT(DISTINCT A.song_id)>=3
