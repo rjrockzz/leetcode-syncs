e62444878169239798efd08ed9cee5ddb5548cc6
codιng вloĸe <rjrockzz@protonmail.com>
2023-02-03 09:49:12 +0530

    Time: 854 ms (42.22%), Space: 0B (100.00%) - LeetHub

--- 2066-account-balance/2066-account-balance.sql
+++ 2066-account-balance/2066-account-balance.sql
@@ -1,3 +1,14 @@
 # Write your MySQL query statement below
-SELECT account_id, day, sum(case when type = 'Deposit' then amount else -amount end) over(partition by account_id order by day asc) as balance 
-FROM transactions
+/*
+Transactions
+* account_id
+* day
+* type
+* amount
+
+
+Solution -
+1. Identify each types, and assign them a positive or negative
+2. Take a running sum on that assignment
+*/
+select account_id, day, sum(case when type = "Deposit" then amount else -amount end) over (partition by account_id order by day) as balance from transactions
