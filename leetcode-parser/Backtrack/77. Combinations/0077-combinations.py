ead7d6683f1d0c3fa8176034c763eac15a22a140
codιng вloĸe <rjrockzz@protonmail.com>
2023-08-01 10:30:47 +0530

    committing files

---
+++ leetcode-parser/Backtrack/77. Combinations/0077-combinations.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        def backtrack(curr, first_num):
+            if len(curr) == k:
+                ans.append(curr[:])
+                return
+
+            need = k - len(curr)
+            remain = n - first_num + 1
+            available = remain - need
+            
+            for num in range(first_num, first_num + available + 1):
+                curr.append(num)
+                backtrack(curr, num + 1)
+                curr.pop()
+
+            return
+        
+        ans = []
+        backtrack([], 1)
+        return ans
