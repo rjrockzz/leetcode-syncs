960895b3a49d8aea662e702c58f720833aa29c8b
codιng вloĸe <rjrockzz@protonmail.com>
2024-02-14 17:33:05 +0530

    Time: 43 ms (87.42%), Space: 17.6 MB (39.20%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return collections.Counter(s) == collections.Counter(t)
+        s_sorted = sorted(s)
+        t_sorted = sorted(t)
+        return s_sorted==t_sorted
